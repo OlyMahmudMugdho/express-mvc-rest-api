fab6ab7a3211d3fc98996b124581fef54774e496
M. Oly Mahmud <mugdhodzs38@gmail.com>
2023-04-15 17:11:48 +0600

    Add files via upload

--- api/posts.js
+++ api/posts.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const AllPostsController = require("../ controllers/AllPostsController");
-
+const CreatePostController = require("../ controllers/CreatePostController");
+const SinglePostController = require("../ controllers/SinglePostController");
 const router = express.Router();
 
 router.route('/')
     .get(AllPostsController.getAllPost)
+    .post(CreatePostController.createPost)
 
+router.route('/:id')
+    .get(SinglePostController.getSinglePost)
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,36 +2,40 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname,'views')));
+app.use(express.static(path.join(__dirname, 'views')));
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json());
 
-
-app.use('/posts',require('./api/posts'))
-
+app.use('/posts', require('./api/posts'));
+app.use('/posts', require('./api/posts'));
+app.use('/posts/:id', require('./api/posts'));
 
 const PORT = process.env.PORT | 5000;
 
 
 const users = [
     {
-        id : 1,
-        firstname : 'john',
-        lastname : 'doe'
+        id: 1,
+        firstname: 'john',
+        lastname: 'doe'
     },
     {
-        id : 2,
-        firstname : 'foo',
-        lastname : 'bar'
+        id: 2,
+        firstname: 'foo',
+        lastname: 'bar'
     }
 ];
 
-app.get('/api',(req,res) => {
+app.get('/api', (req, res) => {
+    console.log(req.body);
     res.json(users);
 })
 
-app.get('/', (req,res) => {
-    res.sendFile(path.join(__dirname,'views','index.html'))
-} )
-.listen(PORT,() => {
-    console.log(`Server running at http://localhost:${PORT}`)
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'index.html'))
 })
+    .listen(PORT, (req, res) => {
+        console.log(req)
+        console.log(`Server running at http://localhost:${PORT}`)
+    })
 
